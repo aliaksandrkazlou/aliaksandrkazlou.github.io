2aa4c0a3b83f8e9dea47526a91b04855a30517ca
Vera Nikalaikina <vnikalaikina@evolutiongaming.com>
2021-11-01 17:49:50 +0300

    Literature

--- public/text_elements.js
+++ public/text_elements.js
@@ -33,6 +33,28 @@ var mainVarsDescriptionAbstract = formula => {
     return `${mainVars}`
 }
 
+var allLiterature = [
+    "\\item Acemoglu, D. (2008). Oligarchic versus democratic societies. Journal of the European Economic Association, 6(1), 1-44.",
+    "\\item Acemoglu, D., \\& Robinson, J. A. (2012). Why nations fail: colon The origins of power, prosperity, and poverty. Currency.",
+    "\\item Alesina, A., \\& Perotti, R. (1996). Income distribution, political instability, and investment. European economic review, 40(6), 1203-1228.",
+    "\\item Benabou, R. (1996). Inequality and growth. NBER macroeconomics annual, 11, 11-74.",
+    "\\item Bourguignon, F., \\& Verdier, T. (2003). Globalisation and endogenous educational responses: The main economic transmission channels. Globalization, Poverty and Inequality, 19-42.",
+    "\\item Galor, O., \\& Zeira, J. (1993). Income distribution and macroeconomics. The review of economic studies, 60(1), 35-52.",
+    "\\item Saint-Paul, G., \\& Verdier, T. (1993). Education, democracy and growth. Journal of development Economics, 42(2), 399-407.",
+    "\\item Schumpeter, J. (1942). Creative destruction. Capitalism, socialism and democracy, 825, 82-85.",
+    "\\item Lindblom, C. E. (1982). Politics and markets. Ethics, 92(4).",
+    "\\item Lizzeri, A., \\& Persico, N. (2004). Why did the elites extend the suffrage? Democracy and the scope of government, with an application to Britain's “Age of Reform”. The Quarterly journal of economics, 119(2), 707-765.",
+    "\\item Persson, T., \\& Tabellini, G. (1994). Representative democracy and capital taxation. Journal of Public Economics, 55(1), 53-70.",
+    "\\item Rao, V. (1984). Democracy and Economic Development. Studies in Comparative International Development 19, 67–81."
+]
+
+var negativeLiterature = [
+    "\\item Schumpeter, J. (1942). Creative destruction. Capitalism, socialism and democracy, 825, 82-85.",
+    "\\item Lindblom, C. E. (1982). Politics and markets. Ethics, 92(4).",
+    "\\item Persson, T., \\& Tabellini, G. (1994). Representative democracy and capital taxation. Journal of Public Economics, 55(1), 53-70.",
+    "\\item Rao, V. (1984). Democracy and Economic Development. Studies in Comparative International Development 19, 67–81."
+]
+
 var abstractPositive = "This article provides evidence that democracy has a significant positive effect on economic growth while controling for"
 var abstractNoEffect = "This article provides evidence that democracy has no significant effect on economic growth while controling for"
 var abstractNegative = "This article provides evidence that democracy has a significant negative effect on economic growth while controling for"
