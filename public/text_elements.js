a066ab92118f3153637572e8bc8c9d08bc86ed4a
Aliaksandr Kazlou <a.v.kazlou@gmail.com>
2021-11-07 15:32:30 +0300

    making se static, smaller step for slider

--- public/text_elements.js
+++ public/text_elements.js
@@ -10,9 +10,9 @@ var chapterOnePositive = "Democratization may foster growth by mitigating capita
 var chapterOneNoEffect = "The connection between political and economic freedom is controversial. Democratization may foster growth by mitigating capital market imperfections  (Galor and Zeira, 1993), or by more effective provision of public goods and education (Bourguignon and Verdier, 2000, Saint-Paul and Verdier, 1993, Benabou, 1996, Lizzeri and Persico, 2004), or by preventing politicians from monopolizing economic opportunities (Acemoglu, 2008, Acemoglu and Robinson, 2012). Increased political stability associated with democratic regimes may also benefit growth by stimulating investment (Alesina and Perotti, 1996). The literature has also indicated several disadvantages of democracy. Many have argued that democracy and growth are contradictory (Lindblom 1977, Schumpeter 1942) Majority voting may result in redistributive taxation which lowers growth rates  (Persson and Tabellini, 1994). Additionally, economic development usually requires unpopular reforms and democratic governments that implemented such reforms would possibly be voted out of office (Rao, 1984)."
 var chapterOneNegative = "The literature has indicated several disadvantages of democracy. Many have argued that democracy and growth are contradictory (Lindblom 1977, Schumpeter 1942) Majority voting may result in redistributive taxation which lowers growth rates  (Persson and Tabellini, 1994). Additionally, economic development usually requires unpopular reforms and democratic governments that implemented such reforms would possibly be voted out of office (Rao, 1984)."
 
-var conflictsVariable = "I also create a measure of social unrest using Clio Infra (clioinfra.eu) indicating participation in internal and external armed conflicts. "
+var conflictsVariable = " I also create a measure of social unrest using Clio Infra (clioinfra.eu) indicating participation in internal and external armed conflicts. "
 var conflictsVariableShort = ", as well as internal and external armed conflicts."
-var mainVariable = "As the main independent variable, I use "
+var mainVariable = "As the main independent variable, I use the democracy index from "
 
 var varsDescription = new Map([
     ["gdp_level", "GPD level"],
